66a8573efed57322a96940e0fa2efe0eded43f32
Rob Roy <=>
2016-03-29 17:01:54 +1100

    Added blog detail pages and CSS for styling

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
